99c1170adf4b6f4abedcb2a956bf22dd692d2f2e
Jascha Muller <jascha.muller@gmail.com>
2019-02-12 22:07:49 +0200

    Further advancements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wgs19',
+	version = '0.1',
+	description = 'The start of worldwide geographic solutions',
+	url = 'https://github.com/JaschaMuller/wgs2019',
+	author = 'SJ Muller',
+	author_email = 'jascha.muller@gmail.com',
+	license = 'None',
+	packages = setuptools.find_packages()
+	)
---
+++ wgs19/__init__.py
@@ -0,0 +1,1 @@
+from .Raster_projection import *
